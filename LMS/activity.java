201df80d80d83435cd363831d813b169c05d3384
JasperJames-j <galang.jasperjames.guianan@gmail.com>
2024-07-06 02:54:33 +0000

    Done

---
+++ LMS/activity.java
@@ -0,0 +1,40 @@
+/*write a program that takes in three numbers from the users and outputs the largest number.
+example: 
+
+Enter three numbers: 7 12 3
+The largest number is: 12
+
+Enter three numbers: 2 2 2
+all numbers are equal
+*/
+
+
+package LMS;
+import java.util.Scanner;
+
+public class activity {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        double num1, num2, num3;
+        System.out.print("Enter three numbers:");
+        num1 = s.nextDouble();
+        num2 = s.nextDouble();
+        num3 = s.nextDouble();
+        
+        System.out.println("The three numbers are: " + num1 +" | " + num2 +" | " + num3);
+
+        if (num1 == num2 && num1 == num3 && num2 == num1 && num2 == num3 && num3 == num1 && num3 == num2) {
+            System.out.println("All numbers are equal"); 
+        }
+        else if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        }
+        else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        }
+        else if (num3 >= num1 && num3 >=num2) {
+            System.out.println("The largest number is: " + num3);
+        }
+    }
+}
---
+++ LMS/task1.java
@@ -0,0 +1,17 @@
+package LMS; 
+public class task1 {
+    
+}
+
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
+    /*public static void main(String[] args) {
+        System.out.println("Hello World!); //Display the string. */
+
+    public static void main(String[] args) {
+        System.out.println('k' + 'i' + 'r' + 'a'); //Display the string.
+    }
+}
